0ef0b1074371fa0db3a684c5238ccaddafc1631c
yil578 <yil578@ucsd.edu>
2021-05-14 12:09:03 -0700

    Trigger Sound with AVFoundation

--- Scrumdinger/MeetingView.swift
+++ Scrumdinger/MeetingView.swift
@@ -6,10 +6,12 @@
 //
 
 import SwiftUI
+import AVFoundation
 
 struct MeetingView: View {
     @Binding var scrum: DailyScrum // Create a scrum binding
-    @StateObject var scrumTimer = ScrumTimer() // Wrapping a property as a @StateObject means the view owns the source of truth for the "object" ("reference type" model). @StateObject ties the ScrumTimer ObservableObject to the MeetingView life cycle.
+    @StateObject var scrumTimer = ScrumTimer() // Wrapping a property as a @StateObject means the view owns the source of truth for the "object" ("reference type" model). @StateObject ties the ScrumTimer ObservableObject to the MeetingView life cycle (keeps the object alive for the life cycle of a view)
+    var player: AVPlayer { AVPlayer.sharedDingPlayer } // the sharedDingPlayer object plays the ding.wav resource
     // requirement to conform to View protocol: body property that returns a View
     var body: some View {
         // use a ZStack to draw a simple shape behind the meeting view
@@ -27,8 +29,15 @@ struct MeetingView: View {
         .padding()
         .foregroundColor(scrum.color.accessibleFontColor)
         .onAppear {
-            scrumTimer.reset(lengthInMinutes: scrum.lengthInMinutes, attendees: scrum.attendees)
             // The timer resets each time an instance of MeetingView shows on screen, indicating that a meeting should begin.
+            scrumTimer.reset(lengthInMinutes: scrum.lengthInMinutes, attendees: scrum.attendees)
+            
+            // ScrumTimer calls this action (a closure) when a speaker’s time expires.
+            scrumTimer.speakerChangedAction = {
+                //trigger audio feedback when a speaker’s time has ended
+                player.seek(to: .zero) // Seeking to time .zero ensures the audio file always plays from the beginning
+                player.play()
+            }
             scrumTimer.startScrum()
         }
         .onDisappear {
